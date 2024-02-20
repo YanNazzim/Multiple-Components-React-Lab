08fbeca7fe42f5a279e815a3dbea5a8737848a9c
Yan <ygonz42@gmail.com>
2024-02-19 23:32:33 -0500

    Included Card css

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <h1>Task: Add three Card elements</h1>
-      <Card h2="First Card's H2" h3="First Card's H3"/>
+      <Card  h2="First Card's H2" h3="First Card's H3"/>
       <Card h2="Second Card's H2" h3="Second Card's H3"/>
       <Card h2="Third Card's H2" h3="Third Card's H3"/>
 
--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,6 @@
 function Card(props) {
     return (
-        <div>
+        <div className="card">
             <h2>{ props.h2 }</h2>
             <h3>{ props.h3 }</h3>
         </div>
