ed2391e87497cb2ffbc07f4a9055090f2f083f0e
Yan <ygonz42@gmail.com>
2024-02-19 23:28:14 -0500

    Lab Finished

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Card from "./Card";
+function App() {
+  return (
+    <div>
+      <h1>Task: Add three Card elements</h1>
+      <Card h2="First Card's H2" h3="First Card's H3"/>
+      <Card h2="Second Card's H2" h3="Second Card's H3"/>
+      <Card h2="Third Card's H2" h3="Third Card's H3"/>
+
+    </div>
+  );
+}
+
+export default App;
